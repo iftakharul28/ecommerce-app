39926b1900df3dc1ba23f2e69e2f413b1eb72781
iftakharul28 <iftakharul28@gmail.com>
2022-10-30 22:46:19 +0600

    url issue solved

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\OngkirController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,50 +26,46 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('homepage');
-Route::get('/shop/{slug?}', [\App\Http\Controllers\ShopController::class, 'index'])->name('shop.index');
-Route::get('/shop/tag/{slug?}', [\App\Http\Controllers\ShopController::class, 'tag'])->name('shop.tag');
-Route::get('/product/{product:slug}', [\App\Http\Controllers\ProductController::class, 'show'])->name('product.show');
+Route::get('/', [HomeController::class, 'index'])->name('homepage');
+Route::get('/shop/{slug?}', [ShopController::class, 'index'])->name('shop.index');
+Route::get('/shop/tag/{slug?}', [ShopController::class, 'tag'])->name('shop.tag');
+Route::get('/product/{product:slug}', [ProductController::class, 'show'])->name('product.show');
 
 // react route
-Route::get('products/{slug?}', [\App\Http\Controllers\ShopController::class, 'getProducts']);
-Route::get('products', [\App\Http\Controllers\HomeController::class, 'getProducts']);
-Route::get('product-detail/{product:slug}', [\App\Http\Controllers\ProductController::class, 'getProductDetail']);
-Route::post('carts', [\App\Http\Controllers\CartController::class, 'store']);
-Route::get('carts', [\App\Http\Controllers\CartController::class, 'showCart']);
+Route::get('products/{slug?}', [ShopController::class, 'getProducts']);
+Route::get('products', [HomeController::class, 'getProducts']);
+Route::get('product-detail/{product:slug}', [ProductController::class, 'getProductDetail']);
+Route::post('carts', [CartController::class, 'store']);
+Route::get('carts', [CartController::class, 'showCart']);
 // ongkir
-Route::get('api/provinces', [\App\Http\Controllers\OngkirController::class, 'getProvinces']);
-Route::get('api/cities', [\App\Http\Controllers\OngkirController::class, 'cities']);
-Route::get('api/shipping-cost', [\App\Http\Controllers\OngkirController::class, 'shippingCost']);
-Route::post('api/set-shipping', [\App\Http\Controllers\OngkirController::class, 'setShipping']);
-Route::post('api/checkout', [\App\Http\Controllers\OrderController::class, 'checkout']);
+Route::get('api/provinces', [OngkirController::class, 'getProvinces']);
+Route::get('api/cities', [OngkirController::class, 'cities']);
+Route::get('api/shipping-cost', [OngkirController::class, 'shippingCost']);
+Route::post('api/set-shipping', [OngkirController::class, 'setShipping']);
+Route::post('api/checkout', [OrderController::class, 'checkout']);
 // get user login
-Route::get('api/users', [\App\Http\Controllers\UserController::class, 'index']);
+Route::get('api/users', [UserController::class, 'index']);
 // ==========
 
 
 Route::group(['middleware' => 'auth'], function() {
-    
-    Route::get('/order/checkout', [\App\Http\Controllers\OrderController::class, 'process'])->name('checkout.process');
-    Route::resource('/cart', \App\Http\Controllers\CartController::class)->except(['store', 'show']);
+    Route::get('/order/checkout', [OrderController::class, 'process'])->name('checkout.process');
+    Route::resource('/cart', CartController::class)->except(['store', 'show']);
 
     Route::group(['middleware' => ['isAdmin'],'prefix' => 'admin', 'as' => 'admin.'], function() {
-        Route::get('dashboard', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
-       
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
         // categories
-        Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class);
-        Route::post('categories/images', [\App\Http\Controllers\Admin\CategoryController::class,'storeImage'])->name('categories.storeImage');
-    
+        Route::resource('categories', CategoryController::class);
+        Route::post('categories/images', [CategoryController::class,'storeImage'])->name('categories.storeImage');
         // tags
-        Route::resource('tags', \App\Http\Controllers\Admin\TagController::class);
-    
+        Route::resource('tags', TagController::class);
         // products
-        Route::resource('products', \App\Http\Controllers\Admin\ProductController::class);
-        Route::post('products/images', [\App\Http\Controllers\Admin\ProductController::class,'storeImage'])->name('products.storeImage');
+        Route::resource('products', ProductController::class);
+        Route::post('products/images', [ProductController::class,'storeImage'])->name('products.storeImage');
     });
 });
 
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
